c6a8ebae89bc9ef0c48464994a17354bea5ab055
IEUser <davidmakowski00@gmail.com>
2020-03-12 11:00:36 -0700

    capstone finished

--- Part6_capstoneProject/capstone/src/flaskr/__init__.py
+++ Part6_capstoneProject/capstone/src/flaskr/__init__.py
@@ -4,92 +4,132 @@ from flask_migrate import Migrate
 import json
 
 from models import setup_db, Player, Team, db
+from auth import AuthError, requires_auth
 
-app = Flask(__name__)
+""" app = Flask(__name__)
 setup_db(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db) """
 
-@app.route('/players', methods=['GET'])
-def get_players():
-    players = Player.query.all()
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    db.create_all()
+    #migrate = Migrate(app, db)
 
-    outplayers = [player.format() for player in players]
 
-    return jsonify({
-        'success': True,
-        'players': outplayers
-    })
+    @app.route('/players', methods=['GET'])
+    @requires_auth('get:players')
+    def get_players(payload):
+        players = Player.query.all()
 
-@app.route('/teams', methods=['GET'])
-def get_teams():
-    teams = Team.query.all()
+        outplayers = [player.format() for player in players]
 
-    outteams = [team.format() for team in teams]
+        return jsonify({
+            'success': True,
+            'players': outplayers
+        })
 
-    return jsonify({
-        'success':True,
-        'teams':outteams
-    })
+    @app.route('/teams', methods=['GET'])
+    @requires_auth('get:teams')
+    def get_teams(payload):
+        teams = Team.query.all()
 
-@app.route('/players', methods=['POST'])
-def create_player():
-    try:
-        body = request.get_json()
-
-        new_name = body.get('name', None)
-        new_skill = body.get('skill', None)
-
-        player = Player(name=new_name, skill=new_skill)
-        player.insert()
+        outteams = [team.format() for team in teams]
 
         return jsonify({
             'success':True,
-            'player':player.format()
+            'teams':outteams
         })
-    except:
-        abort(422)
 
-@app.route('/teams/<int:team_id>', methods=['PATCH'])
-def update_team(team_id):
-    try:
-        old_team = Team.query.filter(Team.id == team_id).one_or_none()
+    @app.route('/players', methods=['POST'])
+    @requires_auth('post:players')
+    def create_player(payload):
+        try:
+            body = request.get_json()
 
-        new_team = request.get_json()
+            print(body)
+            new_name = body.get('name', None)
+            new_skill = body.get('skill', None)
 
-        new_name = new_team.get('name', None)
-        new_city = new_team.get('city', None)
+            player = Player(name=new_name, skill=new_skill)
+            player.insert()
 
-        if new_name is not None:
-            old_team.name = new_name
-        if new_city is not None:
-            old_team.city = new_city
+            return jsonify({
+                'success':True,
+                'player':player.format()
+            })
+        except:
+            abort(422)
 
-        old_team.update()
+    @app.route('/teams/<int:team_id>', methods=['PATCH'])
+    @requires_auth('patch:team')
+    def update_team(payload, team_id):
+        try:
+            old_team = Team.query.filter(Team.id == team_id).one_or_none()
 
-        return jsonify({
-            'success': True,
-            'team': old_team.format()
-        })
-    
-    except:
-        abort(422)
+            new_team = request.get_json()
 
-@app.route('/players/<int:player_id>', methods=['DELETE'])
-def delete_player(player_id):
-    try:
-        player = Player.query.filter(Player.id == player_id).one_or_none()
+            new_name = new_team.get('name', None)
+            new_city = new_team.get('city', None)
 
-        if player is None:
-            abort(404)
+            if new_name is not None:
+                old_team.name = new_name
+            if new_city is not None:
+                old_team.city = new_city
+
+            old_team.update()
+
+            return jsonify({
+                'success': True,
+                'team': old_team.format()
+            })
         
-        player.delete()
+        except:
+            abort(422)
 
+    @app.route('/players/<int:player_id>', methods=['DELETE'])
+    @requires_auth('delete:player')
+    def delete_player(payload, player_id):
+        try:
+            player = Player.query.filter(Player.id == player_id).one_or_none()
+
+            if player is None:
+                abort(404)
+            
+            player.delete()
+
+            return jsonify({
+                'success': True,
+                'delete_id': player_id
+            })
+        except:
+            abort(422)
+
+    #error handlers
+    @app.errorhandler(405)
+    def not_allowed(error):
         return jsonify({
-            'success': True,
-            'delete_id': player_id
+            "success":False,
+            "error":405,
+            "message":"not allowed"
         })
-    except:
-        abort(422)
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success":False,
+            "error":422,
+            "message":"unprocessable"
+        })
+
+    @app.route('/authTest')
+    @requires_auth('post:players')
+    def test_auth(payload):
+        print('executing test auth')
+        print(payload)
+        return 'Testin auth system'
+
+    return app
 
 
         
--- Part6_capstoneProject/capstone/src/models.py
+++ Part6_capstoneProject/capstone/src/models.py
@@ -9,7 +9,7 @@ database_path = "postgres://{}/{}".format('postgres:1234@localhost:5432', databa
 
 db = SQLAlchemy()
 
-def setup_db(app):
+def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
